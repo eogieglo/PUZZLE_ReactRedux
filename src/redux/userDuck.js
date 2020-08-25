4cc2cdd7e310c95d63c4695df965488e0fe6a39a
eogieglo <eric.ogieglo@gmail.com>
2020-08-25 16:20:10 -0300

    Creando el action para hacer el login y respaldar el store

--- src/redux/userDuck.js
+++ src/redux/userDuck.js
@@ -1,18 +1,53 @@
+import { loginWithGoogle } from '../firebase'
+
 //constanst
 let initialData = {
-    loggedIn: false
+    loggedIn: false,
+    fetching: false
 }
 
 let LOGIN = "LOGIN"
-
+let LOGIN_SUCESS = "LOGIN_SUCESS" 
+let LOGIN_ERROR = "LOGIN_ERROR"
 
 //reducer
 export default function reducer(state = initialData, action){
     switch(action.type){
+        case LOGIN_SUCESS:
+            return {...state, fetching:false, ...action.payload }
+        case LOGIN_ERROR:
+            return {...state, fetching:false, error:action.payload }
         case LOGIN:
         default:
             return state
     }
 }
 
+//Funcion auxiliar
+
+function saveStorage(storage){
+    localStorage.storage = JSON.stringify(storage)
+}
+
 //action
+
+export let doGoogleLoginAction = () => (dispatch, getState) => {
+    dispatch({
+        type: LOGIN
+    })
+    return loginWithGoogle()
+        .then(user =>{
+            dispatch({
+                type: LOGIN_SUCESS,
+                payload: { ...user }
+            })
+            saveStorage(getState)
+        })
+        .catch(err => {
+            console.log(err)
+            dispatch ({
+                type: LOGIN_ERROR,
+                payload: err.message
+            })       
+        }) 
+}
