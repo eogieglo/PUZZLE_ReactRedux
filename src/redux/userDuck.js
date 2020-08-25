8d18df2b3e5363f67017e24f9e0520e16064866f
eogieglo <eric.ogieglo@gmail.com>
2020-08-24 21:16:41 -0300

    Que es un Duck <--]

---
+++ src/redux/userDuck.js
@@ -0,0 +1,18 @@
+//constanst
+let initialData = {
+    loggedIn: false
+}
+
+let LOGIN = "LOGIN"
+
+
+//reducer
+export default function reducer(state, action){
+    switch(action.type){
+        case LOGIN:
+        default:
+            return state
+    }
+}
+
+//action
