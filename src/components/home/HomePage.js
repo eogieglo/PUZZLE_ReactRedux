52956e5000ddab99e69deffd161b1e2e26b0f7c7
eogieglo <eric.ogieglo@gmail.com>
2020-08-25 15:42:43 -0300

    Conectando componentes a Redux para obtener acciones

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -3,14 +3,19 @@ import Card from '../card/Card'
 import styles from './home.module.css'
 import axios from 'axios'
 import { connect } from 'react-redux'
+import { removeCharactersAction } from '../../redux/charsDuck'
 
-function Home({chars}) {
+function Home({chars, removeCharactersAction }) {
 
     function renderCharacter() {
         let char = chars[0]
         return (
-            <Card {...char} />
+            <Card leftClick={nextCharacter} {...char} />
         )
+    }
+
+    function nextCharacter(){
+        removeCharactersAction()
     }
 
     return (
@@ -29,4 +34,4 @@ function mapState(state){
     }
 }
 
-export default connect(mapState)(Home)
+export default connect(mapState, {removeCharactersAction})(Home)
