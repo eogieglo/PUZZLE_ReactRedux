42e722ccd52e0f28fdb2232c161a3b5618866e8a
eogieglo <eric.ogieglo@gmail.com>
2020-08-26 00:13:40 -0300

    Middleware para las rutas

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,28 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import Home from './components/home/HomePage'
 import FavPage from './components/favs/FavPage'
 import LoginPage from './components/login/LoginPage'
 
+function PrivateRoute({ path, component, ...rest }){
+    let storage = localStorage.getItem('storage')
+    //storage = JSON.parse(storage)
+    if(storage && storage.user) 
+    {
+        return <Route path={path} component={component} {...rest}/>
+    }
+    else{
+        return <Redirect to="/login" {...rest} />
+    }
+}
+
+//NO ME FUNCIONO LOS LINKS DE INICIO Y LOGIN LUEGO DE INICIAR SESION, quedan en blanco y por console tengo un error de 
+
 export default function Routes() {
     return (
         <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/favs" component={FavPage} />
+            <PrivateRoute exact path="/" component={Home} />
+            <PrivateRoute path="/favs" component={FavPage} />
             <Route path="/login" component={LoginPage} />
         </Switch>
     )
